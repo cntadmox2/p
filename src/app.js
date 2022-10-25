9f9792e016aa88d98b0ec06f9662e06f08885e6a
cntadmox2 <cntadmox@hotmail.com>
2022-10-25 10:27:42 -0600

    random no mimporta

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors({origin:whiteList}))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.get('/', (req, res) => {
-  res.send('GET request to the homepage')
+  res.sendFile(path.resolve('src/uploads/1663563087069.jpg'))
 })
 app.use('/galeria', router)
 
