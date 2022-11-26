a08f5974db1f4d3048e061c12279a26fb4a27f95
cntadmox2 <cntadmox@hotmail.com>
2022-11-26 10:32:48 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ setInterval(() => {
             return console.error(err);
         }
         now = new Date().getTime();
-        endTime = new Date(stat.ctime).getTime() + 50000;
+        endTime = new Date(stat.ctime).getTime() + 60000;
         if (now > endTime) {
             return rimraf(path.join(uploadsDir, file), function(err) {
             if (err) {
