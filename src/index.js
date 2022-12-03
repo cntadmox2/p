313a6dc8f3354f954ce09979312b4328ea1284b2
cntadmox2 <cntadmox@hotmail.com>
2022-12-03 16:19:40 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,10 +12,6 @@ const port =  process.env.PORT || 3100;
 const connectDB = async()=>{
     try {
         await mongoose.connect(process.env.MONGODB_URI)  
-            
-        //'mongodb://localhost:27017/miweb',{ useNewUrlParser:true, useUnifiedTopology: true })
-
-        // 'mongodb+srv://cntadmox:xaIG9SuECizZbbUQ@cluster0.qvnkqen.mongodb.net/?retryWrites=true&w=majority')   
          
         app.listen(port, () => console.log("Puerto==> ",port));
     } catch (error) {
